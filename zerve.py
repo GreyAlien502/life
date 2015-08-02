e8aacab6f292c05c5d27af20f0b14dab6210edf3
GreyAlien502 <carlos.a@del-castillo.net>
2015-08-02 14:25:32 +0000

    everything sortof works

--- zerve.py
+++ zerve.py
@@ -17,19 +17,15 @@ def checkCreds(username,password):
 		return False
 
 def send(destination,content):
-	def rawChat(to,message):
-		print(message)
-		options = 'cmd=chat&to='+to+'&content='+message
-		return base._get(options,True)
-	content=urllib.parse.urlencode({"content":content})[9:]
 	if len(content) < 8000:
-		chat(None,destination,'0'+content)
+		base.chat(None,destination,'0'+content)
 	else:
-		blocked_size = 5000*math.floor(len(content)/5000)
-		lecian = [content[i:i+5000] for i in range(0,len(content),5000)]
+		block_size = 2300
+		blocked_size = block_size*math.floor(len(content)/block_size)
+		lecian = [content[i:i+block_size] for i in range(0,len(content)-block_size,block_size)]
 		for lecian_item in lecian:
-			rawChat(destination,'1'+lecian_item)
-		rawChat(destination,'0'+content[blocked_size:])
+			base.chat(None,destination,'1'+lecian_item)
+		base.chat(None,destination,'0'+content[blocked_size:])
 
 
 
@@ -61,7 +57,7 @@ while True:
 						if command == 'login':
 							output = '<h1>Welcome. Enjoy your premium <a href="content.html">membership.</a></h1>'
 						if command == 'home':
-							output = '<a href="../index.html">Home</a><br>Hello '+username+'. Enjoy the site.<br><input type="button" onclick="messageboard()"><br>'+3000*'(^_^)'
+							output = '<a href="../index.html">Home</a><br>Hello '+username+'<br><a onclick="posts()">Posts</a> <a onclick="files()">Personal Files</a> <a onclick="logout()">Logout</a> <a onclick="messages"> Messages</a> <a onclick="seeBash()">Bash</a> <a onlick="preferences()">Preferences</a>'
 					else:
 						output = 'You have not entered a correct password. You can log in <a href="login.html">here</a>. If you do not have an account or can not remember the password feel free to use the guest account.'
 					commands.connect(sendback,sendback+'_cat')
