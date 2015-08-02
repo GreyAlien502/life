4608d10858b79e5014633cf5c806027ee8eaf5a7
GreyAlien502 <carlos.a@del-castillo.net>
2015-08-02 07:09:06 +0000

    rawchat for reference

--- basiccommands.py
+++ basiccommands.py
@@ -1,26 +1,27 @@
 import urllib.request
 import json
 
-def __get(options):
-	return json.loads(urllib.request.urlopen('http://ec.androiddown.com/chat/app.php?'+urllib.parse.urlencode(options)).read().decode('UTF-8'))
+def _get(options,preencoded=False):
+	if preencoded: return json.loads(urllib.request.urlopen('http://ec.androiddown.com/chat/app.php?'+options                        ).read().decode('UTF-8'))
+	else:          return json.loads(urllib.request.urlopen('http://ec.androiddown.com/chat/app.php?'+urllib.parse.urlencode(options)).read().decode('UTF-8'))
 
 def login(ID=None):
 	options = {'cmd':'login'}
 	if ID != None:
 		options.update({'id':ID})
-	return __get(options)
+	return _get(options)
 def keep(ID=None):
 	options = {'cmd':'keep'}
 	if ID != None:
 		options.update({'id':ID})
-	return __get(options)
+	return _get(options)
 def disconnect(ID='',to=''):
 	options = {'cmd':'disconnect'}
 	if ID != None:
 		options.update({'id':ID})
 	if to != None:
 		options.update({'to':to})
-	return __get(options)
+	return _get(options)
 def chat(ID=None,to=None,message=None):
 	options = {'cmd':'chat'}
 	if ID != None:
@@ -29,5 +30,5 @@ def chat(ID=None,to=None,message=None):
 		options.update({'to':to})
 	if message != None:
 		options.update({'content':message})
-	return __get(options)
+	return _get(options)
 	
--- zerve.py
+++ zerve.py
@@ -1,7 +1,12 @@
+import math
 import time
 import os
+import json
+import urllib
+
 import commands
 import basiccommands as base
+
 
 def checkCreds(username,password):
 	userfile = open(os.getenv('HOME')+"/.zerved/accounts",'r')
@@ -10,6 +15,21 @@ def checkCreds(username,password):
 		return True
 	else:
 		return False
+
+def send(destination,content):
+	def rawChat(to,message):
+		print(message)
+		options = 'cmd=chat&to='+to+'&content='+message
+		return base._get(options,True)
+	content=urllib.parse.urlencode({"content":content})[9:]
+	if len(content) < 8000:
+		chat(None,destination,'0'+content)
+	else:
+		blocked_size = 5000*math.floor(len(content)/5000)
+		lecian = [content[i:i+5000] for i in range(0,len(content),5000)]
+		for lecian_item in lecian:
+			rawChat(destination,'1'+lecian_item)
+		rawChat(destination,'0'+content[blocked_size:])
 
 
 
@@ -39,12 +59,14 @@ while True:
 
 					if (checkCreds(username,password)):
 						if command == 'login':
-							output = '<h1>Welcome. Enjoy your premium <a href="content">membership.</a></h1>'
+							output = '<h1>Welcome. Enjoy your premium <a href="content.html">membership.</a></h1>'
+						if command == 'home':
+							output = '<a href="../index.html">Home</a><br>Hello '+username+'. Enjoy the site.<br><input type="button" onclick="messageboard()"><br>'+3000*'(^_^)'
 					else:
-						output = 'You are not logged in. You can log in <a href="login.html">here</a>. If you do not have an account or can not remember the password feel free to use the guest account.'
-					commands.connect(name,name+'_cat')
-					base.chat(None,name,output)
+						output = 'You have not entered a correct password. You can log in <a href="login.html">here</a>. If you do not have an account or can not remember the password feel free to use the guest account.'
+					commands.connect(sendback,sendback+'_cat')
+					send(sendback,json.dumps(output))
 			elif event['type'] == 'question':
 				1
-	time.sleep(1)
+	#time.sleep(.05)
 
